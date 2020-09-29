e6311ef3840c65f4933b58a06b10c0e55cbc3e8e
SemicolonExpected <semicolonexpected@gmail.com>
2020-09-29 07:16:49 -0400

    Changed pip to pip3, and fixed some pep8 violations while ignoring some pep8 violations

--- source/boot.py
+++ source/boot.py
@@ -7,58 +7,67 @@ from flask import request
 
 from flask_restplus import Resource, Api
 
-app = Flask(__name__)                  #  Create a Flask WSGI application
-api = Api(app)                         #  Create a Flask-RESTPlus API
+app = Flask(__name__)                  # Create a Flask WSGI application
+api = Api(app)                         # Create a Flask-RESTPlus API
 
-@api.route('/hello')                   #  Create a URL route to this resource
-class HelloWorld(Resource):            #  Create a RESTful resource
-    def get(self):                     #  Create GET endpoint
-        return {'hello': 'world'}
+
+@api.route('/hello')                   # Create a URL route to this resource
+class HelloWorld(Resource):            # Create a RESTful resource
+	def get(self):                     # Create GET endpoint
+		return {'hello':'world'}
 
 
 @api.route('/task/create', methods=['GET', 'POST'])
-def createTask(): #i honestly dont know what to do here yet
+def createTask():  # i honestly dont know what to do here yet
 	if request.method == 'POST':
 		return {'create': 'task'}
 	else:
-		return {'Show' : 'Form'}
+		return {'Show':'Form'}
+
 
 @api.route('/task/')
 @api.route('/task/<int:task_id>')
 def getTask(task_id = -1):
 	return 'Task %d' % task_id
 
+
 @api.route('/task/edit/<int:task_id>', methods=['GET', 'POST'])
-def updateTask(task_id): #i honestly dont know what to do here yet
+def updateTask(task_id):  # i honestly dont know what to do here yet
 	if request.method == 'POST':
-		return {'update': 'task'}
+		return {'update':'task'}
 	else:
-		return {'Show' : 'Form'}
+		return {'Show': 'Form'}
+
 
 @api.route('/task/delete/<int:task_id>', methods=['PUT', 'DELETE'])
 def deleteTask(task_id):
 	return 'Task %d' % task_id
 
+
 @api.route('/CreateUser', methods=['GET', 'POST'])
-def createUser(): #i honestly dont know what to do here yet
+def createUser():  # i honestly dont know what to do here yet
 	if request.method == 'POST':
-		return {'create': 'user'}
+		return {'create':'user'}
 	else:
-		return {'Show' : 'Form'}
+		return {'Show':'Form'}
+
 
 @api.route('/user/')
 @api.route('/user/<int:user_id>')
 def getUser(user_id = -1):
 	return 'User %d' % user_id
 
-@api.route('/user/edit/<int:user_id>', methods=['GET', 'POST'])
-def updateTask(user_id): #i honestly dont know what to do here yet
-	if request.method == 'POST':
-		return {'update': 'user'}
-	else:
-		return {'Show' : 'Form'}
 
-@api.route('/user/delete/<int:user_id>', methods=['PUT', 'DELETE']) #I dont know if I want to do DELETE or PUT
+@api.route('/user/edit/<int:user_id>', methods=['GET', 'POST'])
+def updateUser(user_id):  # i honestly dont know what to do here yet
+	if request.method == 'POST':
+		return {'update':'user'}
+	else:
+		return {'Show':'Form'}
+
+
+@api.route('/user/delete/<int:user_id>', methods=['PUT', 'DELETE'])
+# I dont know if I want to do DELETE or PUT
 def deleteUser(user_id):
 	return 'User %d' % user_id
 
@@ -69,8 +78,9 @@ def deleteUser(user_id):
 
 #def main():
 #    initialize_app(app)
-#    app.run(debug=True) #  Start a development server
+#    app.run(debug=True) # Start a development server
+
 
 if __name__ == '__main__':
-	app.run(debug=True)	#  Start a development server
-    #main()               
+	app.run(debug=True)	 # Start a development server
+	#main()
