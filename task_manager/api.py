a5b87f8f0d90ff07ee105116b981824e1fc8a0b4
SemicolonExpected <semicolonexpected@gmail.com>
2020-09-30 16:00:00 -0400

    added endpoints that work

--- task_manager/api.py
+++ task_manager/api.py
@@ -2,10 +2,15 @@
 Bootstrap program
 '''
 
-from flask import Flask
-from flask import request
+#import os
+from flask import Flask, Blueprint
+#from flask import request
 
 from flask_restx import Resource, Api
+
+from task_manager.routes.tasks import task as task_namespace
+from task_manager.routes.users import user as user_namespace
+
 
 app = Flask(__name__)                  # Create a Flask WSGI application
 api = Api(app)                         # Create a Flask-RESTPlus API
@@ -20,16 +25,10 @@ class HelloWorld(Resource):            # Create a RESTful resource
 @api.route('/task/create', methods=['GET', 'POST'])
 class CreateTask(Resource):
 	def get(self):
-		return {'create': 'task'}
+		return {'Show': 'Form'}
 
 	def post(self):
-		return {'Show':'Form'}
-
-	#def createTask():  # i honestly dont know what to do here yet
-	#	if request.method == 'POST':
-	#		return {'create': 'task'}
-	#	else:
-	#		return {'Show':'Form'}
+		return {'Create':'Task'}
 
 
 @api.route('/task/')
@@ -41,16 +40,16 @@ class GetTask(Resource):
 
 @api.route('/task/edit/<int:task_id>', methods=['GET', 'POST'])
 class UpdateTask(Resource):
-	def updateTask(task_id):  # i honestly dont know what to do here yet
-		if request.method == 'POST':
-			return {'update':'task'}
-		else:
-			return {'Show': 'Form'}
+	def get(self, task_id):
+		return {'Show': 'Form'}
+
+	def post(self, task_id):
+		return {'Update':'Task'}
 
 
 @api.route('/task/delete/<int:task_id>', methods=['PUT', 'DELETE'])
 class DeleteTask(Resource):
-	def deleteTask(task_id):
+	def delete(self, task_id):
 		return 'Task %d' % task_id
 
 
@@ -61,11 +60,6 @@ class CreateUser(Resource):
 
 	def post(self):
 		return {'create':'user'}
-	#def createUser():  # i honestly dont know what to do here yet
-	#	if request.method == 'POST':
-	#		return {'create':'user'}
-	#	else:
-	#		return {'Show':'Form'}
 
 
 @api.route('/user/')
@@ -79,29 +73,33 @@ class GetUser(Resource):
 
 @api.route('/user/edit/<int:user_id>', methods=['GET', 'POST'])
 class UpdateUser(Resource):
-	def updateUser(user_id):  # i honestly dont know what to do here yet
-		if request.method == 'POST':
-			return {'update':'user'}
-		else:
-			return {'Show':'Form'}
+	def get(self):
+		return {'Show':'Form'}
+
+	def post(self):
+		return {'update':'user'}
 
 
 @api.route('/user/delete/<int:user_id>', methods=['PUT', 'DELETE'])
 # I dont know if I want to do DELETE or PUT
 class DeleteUser(Resource):
-	def deleteUser(user_id):
+	def delete(user_id):
 		return 'User %d' % user_id
 
-#def initialize_app(flask_app):
 
-	#api.add_namespace()
-	#api.add_namespace()
+def initialize_app(flask_app):
+	blueprint = Blueprint('api', __name__, url_prefix = '/routes')
+	api.init_app(blueprint)
+	api.add_namespace(task_namespace)
+	api.add_namespace(user_namespace)
+	flask_app.register_blueprint(blueprint)
 
-#def main():
-#    initialize_app(app)
-#    app.run(debug=True) # Start a development server
+
+def main():
+	initialize_app(app)
+	app.run(debug=True)  # Start a development server
 
 
 if __name__ == '__main__':
-	app.run(debug=True)	 # Start a development server
-	#main()
+	#app.run(debug=True)	 # Start a development server
+	main()
---
+++ task_manager/routes/users.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from flask import request
+
+from flask_restx import Resource, Api
+
+api = Api(version = 'alpha', title = 'Task Manager', description = 'Another Productivity App')  
+
+user = api.namespace('user', description = "User Related Functions")
