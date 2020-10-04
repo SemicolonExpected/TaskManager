75498e1ade66dc89681d60365f6cc0470e44a766
SemicolonExpected <semicolonexpected@gmail.com>
2020-10-04 01:58:23 -0400

    Dummy test with trivial style fixes

--- task_manager/routes/users.py
+++ task_manager/routes/users.py
@@ -3,6 +3,6 @@ from flask import request
 
 from flask_restx import Resource, Api
 
-api = Api(version = 'alpha', title = 'Task Manager', description = 'Another Productivity App')  
+api = Api(version = 'alpha', title = 'Task Manager', description = 'Another Productivity App')
 
 user = api.namespace('user', description = "User Related Functions")
