3cc3ba6f6c57dece2bc06b7ecf620f2bd8e42c81
Alex Hong <ah4927@nyu.edu>
2020-10-15 19:17:02 -0400

    lint fixes on ./routes/users.py

--- task_manager/routes/users.py
+++ task_manager/routes/users.py
@@ -8,30 +8,30 @@ api = Api(user_bp)
 
 @api.route('/create', methods=['GET', 'POST'])
 class CreateUser(Resource):
-	def get(self):
-		return {'Show' :'Form'}
+    def get(self):
+        return {'Show': 'Form'}
 
-	def post(self):
-		return {'create' :'user'}
+    def post(self):
+        return {'create': 'user'}
 
 
 @api.route('/')
 @api.route('/<int:user_id>')
 class FetchUser(Resource):
-	def get(self, user_id = -1):
-		return {'User': user_id}
+    def get(self, user_id=-1):
+        return {'User': user_id}
 
 
 @api.route('/edit/<int:user_id>', methods=['GET', 'POST'])
 class UpdateUser(Resource):
-	def get(self):
-		return {'Show' :'Form'}
+    def get(self):
+        return {'Show': 'Form'}
 
-	def post(self):
-		return {'update' :'user'}
+    def post(self):
+        return {'update': 'user'}
 
 
 @api.route('/delete/<int:user_id>', methods=['PUT', 'DELETE'])
 class DeleteUser(Resource):
-	def delete(self ,user_id):
-		return 'User %d' % user_id
+    def delete(self, user_id):
+        return 'User %d' % user_id
