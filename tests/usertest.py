6957d4aae10233ff837db2a199dad5902064bf02
semicolonexpected <semicolonexpected@gmail.com>
2020-10-19 00:36:06 -0400

    added

---
+++ tests/usertest.py
@@ -0,0 +1,21 @@
+import unittest
+
+class Test_User(unittest.TestCase):
+	def setup(self):
+		pass
+
+
+	def createTest(self):
+		pass
+
+
+	def getTest(self):
+		pass
+
+	
+	def updateTest(self):
+		pass
+
+
+	def deleteTest(self):
+		pass
