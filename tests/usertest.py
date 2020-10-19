913f66d255388b2abb4b50ef3985e264804843fa
semicolonexpected <semicolonexpected@gmail.com>
2020-10-19 00:37:11 -0400

    fixed some linting errors
    Added unittest stubs

--- tests/usertest.py
+++ tests/usertest.py
@@ -4,18 +4,14 @@ class Test_User(unittest.TestCase):
 	def setup(self):
 		pass
 
-
 	def createTest(self):
 		pass
-
 
 	def getTest(self):
 		pass
 
-	
 	def updateTest(self):
 		pass
-
 
 	def deleteTest(self):
 		pass
