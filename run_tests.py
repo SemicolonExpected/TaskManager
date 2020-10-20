adef10c18d8f7ecbd45d2b408497a6c18c3821e5
semicolonexpected <semicolonexpected@gmail.com>
2020-10-20 09:01:11 -0400

    using test runner from unittest

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,6 @@
 import unittest
 
 if __name__ == '__main__':
-    suite = unittest.defaultTestLoader.discover('tests')
+    suite = unittest.defaultTestLoader.discover('task_manager/tests')
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
