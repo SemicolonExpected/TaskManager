b98d67bef2c6b685710774e811a0f72bbb26a90e
Alex Hong <ah4927@nyu.edu>
2020-10-24 10:31:46 -0400

    secret_key logic

--- config.py
+++ config.py
@@ -2,18 +2,18 @@ from os import environ, path
 from dotenv import load_dotenv
 
 basedir = path.abspath(path.dirname(__file__))
-#load_dotenv(path.join(basedir, '.env'))
+load_dotenv(path.join(basedir, '.env'))
 
 
 class Config:
     """ Set Parent Class config variables """
-    #SECRET_KEY = environ.get('SECRET_KEY')
-    SECRET_KEY = "very_secret_key_1234567"
+    SECRET_KEY = environ.get('SECRET_KEY')
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
 
 
 class DevelopmentConfig(Config):
+    SECRET_KEY = "very_secret_key_1234567"
     FLASK_ENV = 'development'
     DEBUG = True
     TESTING = True
