bb83d6f0d96fc3342f90102248d8f7c334b1d3c6
semicolonexpected <semicolonexpected@gmail.com>
2020-11-07 15:56:40 -0500

    putting stuff in

---
+++ task_manager/tests/test_task.py
@@ -0,0 +1,31 @@
+import unittest
+#import task_manager.routes.tasks
+
+
+class TestTask(unittest.TestCase):
+	def setup(self):
+		pass
+
+	def test_Create(self):
+		'''
+		TEST CREATE TASK
+		'''
+		self.assertEqual('1', '1')
+
+	def test_Get(self):
+		'''
+		TEST FETCH TASK
+		'''
+		self.assertEqual('1', '1')
+
+	def test_Update(self):
+		'''
+		TEST UPDATE TASK
+		'''
+		self.assertEqual('1', '1')
+
+	def test_Delete(self):
+		'''
+		TEST DELETE TASK
+		'''
+		self.assertEqual('1', '1')
---
+++ task_manager/tests/test_user.py
@@ -0,0 +1,31 @@
+import unittest
+#import task_manager.routes.users
+
+
+class TestUser(unittest.TestCase):
+	def setup(self):
+		pass
+
+	def test_Create(self):
+		'''
+		TEST CREATE USER
+		'''
+		self.assertEqual('1', '1')
+
+	def test_Get(self):
+		'''
+		TEST FETCH USER
+		'''
+		self.assertEqual('1', '1')
+
+	def test_Update(self):
+		'''
+		TEST UPDATE USER
+		'''
+		self.assertEqual('1', '1')
+
+	def test_Delete(self):
+		'''
+		TEST DELETE USER
+		'''
+		self.assertEqual('1', '1')
