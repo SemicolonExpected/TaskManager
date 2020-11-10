60732cc0557de86ed8eb212a543e8af935c7d0e5
Alex Hong <ah4927@nyu.edu>
2020-11-10 12:16:58 -0500

    flake 8

--- config.py
+++ config.py
@@ -5,9 +5,10 @@ from dotenv import load_dotenv
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 
+
 class Config:
     """ Set Parent Class config variables """
-    SECRET_KEY = environ['SECRET_KEY']
+    SECRET_KEY = environ['SECRET_KEY'] or "very_secret_key_1234567"
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
