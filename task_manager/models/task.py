51d3c2de928847117dd8b80e860d33d45fb15d7a
Meghana359 <meghana359@gmail.com>
2020-11-17 14:54:11 +0530

    reverted models datetime and assignment comment

--- task_manager/models/task.py
+++ task_manager/models/task.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Text
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Text
 from task_manager import db, ma
 from task_manager.models.user import User
 # User imported for db.relationship
@@ -11,8 +11,8 @@ class Task(db.Model):
     priority = Column(Integer, default=1, nullable=False)
     # do a check to make sure is in range
     description = Column(Text, nullable=True)
-    start_time = Column(String(80), nullable=True)
-    end_time = Column(String(80), nullable=True)
+    start_time = Column(DateTime, nullable=True)
+    end_time = Column(DateTime, nullable=True)
     # make sure end time is after start time
     time_estimate = Column(String(80), nullable=True)
     user_id = db.Column(Integer, ForeignKey('user.id'))
--- task_manager/routes/tasks.py
+++ task_manager/routes/tasks.py
@@ -2,11 +2,11 @@ from flask import request, make_response, render_template
 from flask import jsonify, redirect
 from task_manager.models.task import Task
 
-# from task_manager.models.assignment import Assignment
+from task_manager.models.assignment import Assignment
 from flask_login import current_user
 
 from task_manager import db
-from datetime import datetime  # , date
+from datetime import datetime, date
 from task_manager import ma
 
 
@@ -30,14 +30,16 @@ def model_post_create_task():
         task_start_time = datetime.strptime(task_st, '%Y-%m-%d %H:%M')
     else:
         now = datetime.now()
-        task_start_time = now.strftime("%Y-%m-%d %H:%M:%S")
+        task_start_time = now.strftime("%Y-%m-%d %H:%M")
+        task_start_time = datetime.strptime(task_start_time, '%Y-%m-%d %H:%M')
     if request.form['end_time']:
         task_endTime = request.form['end_time']
         task_end = task_endTime.replace('T', ' ')
         task_end_time = datetime.strptime(task_end, '%Y-%m-%d %H:%M')
     else:
         now = datetime.now()
-        task_end_time = now.strftime("%Y-%m-%d %H:%M:%S")
+        task_end_time = now.strftime("%Y-%m-%d %H:%M")
+        task_end_time = datetime.strptime(task_end_time, '%Y-%m-%d %H:%M')
 
     new_task = Task(title=task_title, priority=task_priority,
                     description=task_decription,
@@ -49,19 +51,20 @@ def model_post_create_task():
         db.session.commit()
 
         '''Add the new task to assignment with the user who created it'''
-        # new_assignment = Assignment(time_added=date.today(),
-        # user_id=current_user.id, task_id=new_task.id)
-        # db.session.add(new_assignment)
-        # db.session.commit()
+        new_assignment = Assignment(time_added=date.today(),
+                                    user_id=current_user.id,
+                                    task_id=new_task.id)
+        db.session.add(new_assignment)
+        db.session.commit()
 
         # flash('Task successfully added!')
 
         return redirect('/tasks')
-        #later this should return /tasks/new_task.id
+        # later this should return /tasks/new_task.id
 
     except Exception as e:
         print(e)
-        return "Task could not be updated :("
+        return "Task could not be created :("
 
 
 def model_fetch_task(task_id):
