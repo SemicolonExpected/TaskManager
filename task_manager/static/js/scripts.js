26e3dabb05a6d5f7272f808c8258953304e5b600
semicolonexpected <semicolonexpected@gmail.com>
2020-12-08 08:46:21 -0500

    fix datetime format

--- task_manager/static/js/scripts.js
+++ task_manager/static/js/scripts.js
@@ -2,5 +2,5 @@ function updateDate(){
 	temp = new Date(start_date.value);
 	timezone_offset = temp.getTimezoneOffset()/60; 
 	temp.setHours(temp.getHours() + (1-timezone_offset));
-	end_date.value = temp.toISOString().split(".")[0];
+	end_date.value = temp.toISOString().split(".")[0].slice(0,-3);
 }
